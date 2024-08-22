0b0ae0f89012d007355ccebab00939e820d1373c
herilalaina <rkt.herilalaina@gmail.com>
2024-08-22 16:24:04 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='lcpfn',
-    version='0.1',
+    name="lcpfn",
+    version="0.1",
     packages=find_packages(),
     entry_points={
-        'console_scripts': [
-            'lcpfn=lcpfn.model:main',
+        "console_scripts": [
+            "lcpfn=lcpfn.model:main",
         ],
     },
 )
